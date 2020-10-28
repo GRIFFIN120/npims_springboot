ca9eabb4b7bf2120c28e3e25614c504675263686
刘寅鹏 <liu_yinpeng120@126.com>
2020-10-28 23:06:38 +0800

    test commit

--- vala-web-npims/src/main/java/com/vala/npims/controller/TestController.java
+++ vala-web-npims/src/main/java/com/vala/npims/controller/TestController.java
@@ -2,8 +2,9 @@ package com.vala.npims.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/test")
 public class TestController {
 
